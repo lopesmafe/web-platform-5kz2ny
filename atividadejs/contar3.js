dc05839665d76b9685cc294ccaca3a13e7db9467
lopesmafe <mflrds@aluno.ifnmg.edu.br>
2022-07-16 14:27:56 -0300

    3 files modified

--- atividadejs/contar3.js
+++ atividadejs/contar3.js
@@ -1,16 +1,16 @@
 function contar() {
   let saida = document.getElementById('saida')
 
-  saida.innerHTML += `<h2>Contando de 1 até 10, marcando os pares</h2>`
+  saida.innerHTML += `<h2>Contando de 100 a 120, marcando os pares</h2>`
 
-  let cont = 10
-  while (cont <= 10) {
+  let cont = 100
+  while (cont <= 120) {
       if (cont % 2 == 0) {
           saida.innerHTML += `<mark><strong> ${cont} </strong></mark>&#x1F449;`    
       } else {
           saida.innerHTML += ` ${cont} &#x1F449;`
       }
-      cont ++ // Corresponde a cont = cont + 1
+      cont ++ 
   }
   saida.innerHTML += ` &#x1F3C1;`
 }

31b006c2fbaef19d006b5edcb9dbec9c45ef170c
lopesmafe <mflrds@aluno.ifnmg.edu.br>
2022-07-16 14:42:55 -0300

    1 file modified

--- atividadejs/contar4.js
+++ atividadejs/contar4.js
@@ -1,8 +1,8 @@
 function contar() {
   let saida = document.getElementById('saida')
 
-  saida.innerHTML += `<h2>Números pares de 1 até 10: </h2>`
-  let cont = 2
+  saida.innerHTML += `<h2>Números pares de 100 até 120: </h2>`
+  let cont = 100
   while (cont <= 120) {
       saida.innerHTML += ` ${cont} &#x1F449;`
       cont += 2 

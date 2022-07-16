c76630c1cc3bf4c5a980aa1396668f696641caaf
lopesmafe <mflrds@aluno.ifnmg.edu.br>
2022-07-16 14:47:43 -0300

    1 file modified

---
+++ atividadejs/contar5.js
@@ -0,0 +1,11 @@
+function contar() {
+  let saida = document.getElementById('saida')
+
+  saida.innerHTML += `<h2>Contando de 10 a 30: </h2>`
+  let cont = 10
+  while (cont >= 30) {
+      saida.innerHTML += ` ${cont} &#x1F449;`
+      cont ++
+  }
+  saida.innerHTML += ` &#x1F3C1;`
+}

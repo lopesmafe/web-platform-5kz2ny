dc810863e5814e9695aafe78c98b9ebfe61811f2
lopesmafe <mflrds@aluno.ifnmg.edu.br>
2022-07-16 14:50:13 -0300

    1 file modified

--- atividadejs/contar5.js
+++ atividadejs/contar5.js
@@ -3,7 +3,7 @@ function contar() {
 
   saida.innerHTML += `<h2>Contando de 10 a 30: </h2>`
   let cont = 10
-  while (cont >= 30) {
+  while (cont <= 30) {
       saida.innerHTML += ` ${cont} &#x1F449;`
       cont ++
   }
